93e00df1635c382c6b73aa4993af711317f13d1c
Nida Batool <nida@example.com>
2024-11-30 19:14:21 +0500

    some change

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import Background from './Components/Background/Background';
+import Navbar from './Components/Navbar/Navbar';
+const App = () => {
+  let heroData =[
+    {text1:'Dive into' ,text2 :'what you love'},
+    {text1:'Indule',text2:'your passions'},
+    {text1:'Give in to',text2:'your passions'},
+  ]
+  const [heroCount,setHeroCount] = useState(2);
+  const[playStatus,setPlayStatus]= useState(false);
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
+      <Hero setPlayStatus={setPlayStatus}
+      heroData={heroData}
+      heroCount={heroCount}
+      setHeroCount={setHeroCount}
+      playStatus={playStatus}
+       />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+import arrow_btn from '../../assets/arrow_btn.png'
+import play_icon from '../../assets/play_icon.png'
+import pause_icon from '../../assets/pause_icon.png'
+
+const Hero = ({heroData,setHeroData,heroCount,setPlayStatus,playStatus}) => {
+  return (
+    <div className="hero">
+         <div className="hero-text">
+          <p></p>
+          <p></p>
+         </div>
+    </div>
+  )
+}
+
+export default Hero
