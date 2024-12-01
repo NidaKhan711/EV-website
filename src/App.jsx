85886049e015c7ef891de7f151a549466ff9a2e5
Nida Batool <nida@example.com>
2024-12-01 18:14:32 +0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Background from './Components/Background/Background';
 import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
 const App = () => {
   let heroData =[
-    {text1:'Dive into' ,text2 :'what you love'},
+    {text1:'Dive into',text2 :'what you love'},
     {text1:'Indule',text2:'your passions'},
     {text1:'Give in to',text2:'your passions'},
   ]
-  const [heroCount,setHeroCount] = useState(2);
+  const [heroCount,setHeroCount] = useState(0);
   const[playStatus,setPlayStatus]= useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeroCount((count) => count === 2 ? 0 : count + 1);
+    }, 5000);
+  
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount}/>
       <Navbar/>
       <Hero setPlayStatus={setPlayStatus}
-      heroData={heroData}
+      heroData={heroData[heroCount]}
       heroCount={heroCount}
       setHeroCount={setHeroCount}
       playStatus={playStatus}
